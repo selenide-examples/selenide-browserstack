7140827230df9b9d6fb5b67dbcc22d765abf154d
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-08-19 21:07:51 +0300

    update test to avoid dependance on 3rd party locators

--- src/test/java/org/selenide/bs/SimpleTest.java
+++ src/test/java/org/selenide/bs/SimpleTest.java
@@ -1,24 +1,20 @@
 package org.selenide.bs;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.partialText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 public class SimpleTest {
-  @Test
-  void search() {
-    open("https://duckduckgo.com");
-    $(By.name("q")).val("Selenide and BrowserStack").pressEnter();
-    $$("[data-testid=\"result\"]").shouldHave(sizeGreaterThan(1));
-    $("[data-testid=\"result\"]").shouldBe(visible).shouldHave(
-      partialText("Run Selenium tests with Selenide"),
-      partialText("BrowserStack Docs"));
+    @Test
+    void search() {
+        open("https://selenide.org");
+        $(".main-menu-pages").find(byText("Users")).click();
 
-  }
+        $$("#user-tags .tag").shouldHave(sizeGreaterThan(8));
+        $$("#selenide-users .user").shouldHave(sizeGreaterThan(20));
+    }
 }
